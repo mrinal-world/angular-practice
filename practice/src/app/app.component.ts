a0d89a37ac748952ddf621ea6c4f32c5b4f80cef
mrinal-world <98207131+mrinal-world@users.noreply.github.com>
2022-07-28 17:11:57 +0530

    Add files via upload

---
+++ practice/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'practice';
+  content = 'Displaying content with one way binding';
+  data = 'Ram and Shyam';
+}
